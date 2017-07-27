e2d25d0281a5f62c8aa90b9f6e010bfdbf1301dd
courtneymcallister <coa.mcallister@gmail.com>
2017-07-27 15:49:12 -0400

    installed axios, express, and request. Set up a Vue instance with dummy data to test webpage, and a sample display instance using placeholders. Used axios to return the number of pages and the image list from one page of info. Added a couple css things to test.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const server = express();
-
 const port = 8080;
+const axios = require('axios');
+const request = require('request');
+
+server.use(express.static(__dirname + '/public'));
 
 server.get('/', function(request, response){
-  response.sendFile('index.html', {root: __dirname + '/public/html'});
+  response.sendFile('index.html', {root: __dirname + '/public/html/'});
 });
+
+const url = 'http://192.168.1.160/pt-dev/graphics_tool/api_passage.php'
+
+//get page count
+axios.post(url, {
+  'command': 'passage_get_page_count'
+})
+.then(res => console.log(res.data.num_pages))
+.catch(err => console.log(err));
+
+//get page data (static page number for now)
+axios.post(url, {
+  'command': 'passage_get_page_data',
+  'page_number': 1
+})
+.then(res => console.log(res.data.image_list))
+.catch(err => console.log(err));
 
 server.listen(port, function(){
   console.log('Now listening on port', port);
---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+
+var images = new Vue({
+  el: '#images',
+  data: {
+    passage_id: 'passage id',
+    item_bank: 'item bank name',
+    usable: 'totes usable',
+    unusable_details: 'the deets',
+    image: 'image link'
+  },
+  methods: {
+
+
+  }
+});
