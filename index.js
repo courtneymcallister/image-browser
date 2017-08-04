cbc8d04a29adc549f5c43b66d25003644dd10a90
courtneymcallister <coa.mcallister@gmail.com>
2017-08-04 13:43:36 -0400

    installed cors. browser now produces a list of the item banks and passage IDs. There are still placeholders for images and links. Added a method to get data for each item on page 1.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const port = 8080;
 const axios = require('axios');
-const request = require('request');
+const cors = require('cors')
 
 server.use(express.static(__dirname + '/public'));
 
@@ -12,20 +12,29 @@ server.get('/', function(request, response){
 
 const url = 'http://192.168.1.160/pt-dev/graphics_tool/api_passage.php'
 
-//get page count
-axios.post(url, {
-  'command': 'passage_get_page_count'
-})
-.then(res => console.log(res.data.num_pages))
-.catch(err => console.log(err));
+//implement cors
+server.use(cors());
 
-//get page data (static page number for now)
-axios.post(url, {
-  'command': 'passage_get_page_data',
-  'page_number': 1
-})
-.then(res => console.log(res.data.image_list))
-.catch(err => console.log(err));
+//TESTING
+// //get page count
+// axios.post(url, {
+//   'command': 'passage_get_page_count'
+// })
+// .then(function(res) {
+//   console.log(res.data);
+// })
+// .catch(err => console.log(err));
+
+
+// //get page data (static page number for now)
+// axios.post(url, {
+//   'command': 'passage_get_page_data',
+//   'page_number': 1
+// })
+// .then(res => res.data.image_list)
+// .catch(err => console.log(err));
+
+
 
 server.listen(port, function(){
   console.log('Now listening on port', port);
--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,45 @@
-
+const url = 'http://192.168.1.160/pt-dev/graphics_tool/api_passage.php'
 var images = new Vue({
   el: '#images',
   data: {
-    passage_id: 'passage id',
-    item_bank: 'item bank name',
+    passageId: 'passage id',
+    itemBank: 'item bank name',
     usable: 'totes usable',
-    unusable_details: 'the deets',
-    image: 'image link'
+    unusableDetails: 'the deets',
+    image: 'image link',
+    totalPages: 10000,
+    currentPage: 1,
+    items: []
   },
   methods: {
+    getTotalPages: function(){
+      axios.post(url, {
+        'command': 'passage_get_page_count'
+      })
+      .then(function(res){
+        images.totalPages = res.data.num_pages;
+      })
+      .catch(err => console.log(err));
+    },
 
-
+    getImageData: function(page){
+      axios.post(url, {
+        'command': 'passage_get_page_data',
+        'page_number': this.currentPage
+      })
+      .then(function(res){
+        console.log(res.data.image_list);
+        var itemData = res.data.image_list;
+        Object.keys(itemData).forEach(function(listItem){
+          images[listItem] = itemData[listItem];
+        })
+        this.items = itemData;
+      }.bind(this))
+      .catch(err => console.log(err));
+    },
+  },
+  created: function(){
+    this.getTotalPages();
+    this.getImageData(this.currentPage);
   }
 });
